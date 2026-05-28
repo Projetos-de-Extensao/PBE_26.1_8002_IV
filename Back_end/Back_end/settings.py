3a2f94a8d6ea05dd7236d12a8c15b21d8958c0af
Rafael <202502905408@alunos.ibmec.edu.br>
2026-05-28 15:16:46 -0300

    segurança com .env e biblioteca

--- Back_end/Back_end/settings.py
+++ Back_end/Back_end/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from decouple import config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa97q+n8*@z_v_r-khz*-p4mmez#m7antmm&+xt=p2*a-5&9ds'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config(
+    'DEBUG',
+    default=False,
+    cast=bool
+)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    cast=Csv()
+)
 
 
 # Application definition
