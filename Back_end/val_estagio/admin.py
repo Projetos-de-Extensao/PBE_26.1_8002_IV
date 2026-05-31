0abdfd1b8ac057f79127483952dd5dbb16f0d17e
Heitor65 <multheitor@gmail.com>
2026-05-31 13:16:17 -0300

    testando alterações no admin

--- Back_end/val_estagio/admin.py
+++ Back_end/val_estagio/admin.py
@@ -7,58 +7,64 @@ from .models import Usuario, Aluno, Secretaria, Coordenador, Curso, Empresa, Tce
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
 
-    fieldsets = (
-        (None, {
-            'fields': (
-                'username',
-                'password',
-            )
-        }),
-
-        ('Informações pessoais', {
-            'fields': (
-                'first_name',
-                'last_name',
-                'email',
-                'unidade',
-            )
-        }),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'username',
-                'password1',
-                'password2',
-                'unidade',
-            ),
-        }),
-    )
-
-    list_display = (
-        'username',
-        'email',
-        'unidade',
-    )
-
-    search_fields = (
-        'username',
-        'email',
-    )
-
-    ordering = ('username',)
-
-    filter_horizontal = ()
-    list_filter = ()
+    list_display = ['username', 'matricula', 'email', 'unidade']
 
 
-admin.site.register(Aluno)
-admin.site.register(Secretaria)
-admin.site.register(Coordenador)
-admin.site.register(Curso)
-admin.site.register(Empresa)
-admin.site.register(Tce)
-admin.site.register(RelatorioSemestral)
-admin.site.register(Estagio)
+
+
+
+    # fieldsets = (
+    #     (None, {
+    #         'fields': (
+    #             'username',
+    #             'password',
+    #         )
+    #     }),
+
+    #     ('Informações pessoais', {
+    #         'fields': (
+    #             'first_name',
+    #             'last_name',
+    #             'email',
+    #             'unidade',
+    #         )
+    #     }),
+    # )
+
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': (
+    #             'username',
+    #             'password1',
+    #             'password2',
+    #             'unidade',
+    #         ),
+    #     }),
+    # )
+
+    # list_display = (
+    #     'username',
+    #     'email',
+    #     'unidade',
+    # )
+
+    # search_fields = (
+    #     'username',
+    #     'email',
+    # )
+
+    # ordering = ('username',)
+
+    # filter_horizontal = ()
+    # list_filter = ()
+
+
+# admin.site.register(Aluno)
+# admin.site.register(Secretaria)
+# admin.site.register(Coordenador)
+# admin.site.register(Curso)
+# admin.site.register(Empresa)
+# admin.site.register(Tce)
+# admin.site.register(RelatorioSemestral)
+# admin.site.register(Estagio)
