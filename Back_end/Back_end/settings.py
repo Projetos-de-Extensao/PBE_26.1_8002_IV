35f951af870f7bfafa8bcb79632b940ef2b8f356
Heitor65 <multheitor@gmail.com>
2026-06-01 13:55:48 -0300

    add: tokenAuthentication no settings

--- Back_end/Back_end/settings.py
+++ Back_end/Back_end/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'val_estagio',
     'rest_framework',
     'corsheaders',
@@ -59,9 +60,19 @@ MIDDLEWARE = [
 CORS_ALLOW_ORIGINS= [
     'http://localhost:3000',
 ]
-
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['content-type', 'authorization']
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = 'Back_end.urls'
 
