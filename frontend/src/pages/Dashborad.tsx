086521e01f3387b6aecc1855e87ce663d539ee04
Heitor <multheitor@gmail.com>
2026-06-10 11:19:50 -0300

    esqueleto projeto react

---
+++ frontend/src/pages/Dashborad.tsx
@@ -0,0 +1,16 @@
+export default function Dashboard() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Sistema de Estágios</h1>
+
+            <p>Bem-vindo ao sistema.</p>
+
+            <div>
+                <button>Alunos</button>
+                <button>Empresas</button>
+                <button>Estágios</button>
+                <button>Relatórios</button>
+            </div>
+        </div>
+    );
+}
